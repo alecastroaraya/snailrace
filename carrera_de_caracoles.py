4b8cb788c5c6dd4f9fdab59a05fac6e09cdba552
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2024-04-01 00:02:33 -0600

    Typo fixes and string formatting fixes
    Fixed some typos and some of the string formatting. Made the finish line farther away to make the races last longer

--- carrera_de_caracoles.py
+++ carrera_de_caracoles.py
@@ -1,3 +1,6 @@
+import os
+import pyfiglet
+
 def tablero_juego():
     """Esta funcion define las variables string usadas para formar el tablero de la carrera.
     - Entrada: Input del usuario.
@@ -15,7 +18,7 @@ def tablero_juego():
     
     import random
     num_tablero = 1
-    linea_meta = "------------META"
+    linea_meta = "----------------------------------------------------------------------------META"
     caracol_1 = "@..1"
     caracol_2 = "@..2"
     caracoles = [caracol_1,caracol_2]
@@ -60,39 +63,53 @@ def tablero_juego():
         global conteo_caracol_2
         global caracoles
         
-        cuenta_tablero = "Tablero "+str(num_tablero)
+        cuenta_tablero = "\nTablero #"+str(num_tablero)+"\n"
         casilla = " "
         casillas_avanzadas = casilla*numero_casillas_escogido
-        
-        
+
         if caracol_escogido == caracol_1:
             conteo_caracol_1 = conteo_caracol_1 + numero_casillas_escogido
             caracol_1 = casillas_avanzadas + caracol_1
             caracoles = [caracol_1,caracol_2]
             num_tablero = num_tablero + 1
-            print(cuenta_tablero,"\n" + caracol_1,"\n" + linea_meta,"\n" + caracol_2,"\n")
-            if conteo_caracol_1 >= 12:
-                print("Felicidades! El caracol #1 ha ganado la carrera!\nVolviendo al menu principal...")
+            print("\n",cuenta_tablero,"\n" + caracol_1,"\n" + linea_meta,"\n" + caracol_2,"\n")
+            if conteo_caracol_1 >= 72:
+                input("\nFELICIDADES!!! El caracol #1 ha ganado la carrera y es oficialmente el caracol mas veloz del planeta!!!\nGanador: Caracol #1\n\nPresione ENTER para volver al menu principal!\n")
                 return menu_juego()
             respuesta = input("Presione ENTER para continuar la carrera.\n")
             nuevo_tablero = carrera_caracoles(random.choice(caracoles),random.randint(0,3))
-            
+
         elif caracol_escogido == caracol_2:
             conteo_caracol_2 = conteo_caracol_2 + numero_casillas_escogido
             caracol_2 = casillas_avanzadas + caracol_2
             caracoles = [caracol_1,caracol_2]
             num_tablero = num_tablero + 1
             print(cuenta_tablero,"\n" + caracol_1,"\n" + linea_meta,"\n" + caracol_2,"\n")
-            if conteo_caracol_2 >= 12:
-                print("Felicidades! El caracol #2 ha ganado la carrera!\nVolviendo al menu principal...")
+            if conteo_caracol_2 >= 72:
+                input("\nFELICIDADES!!! El caracol #2 ha ganado la carrera y es oficialmente el caracol mas veloz del planeta!!!\nGanador: Caracol #2\n\nPresione ENTER para volver al menu principal!\n")
                 return menu_juego()
-            respuesta = input("Presione ENTER para continuar la carrera.\n")
+            respuesta = input("\nPresione ENTER para continuar la carrera.\n")
             nuevo_tablero = carrera_caracoles(random.choice(caracoles),random.randint(0,3))
-            
+
         else:
             carrera_caracoles_aux(random.choice(caracoles),random.randint(0,3))
-            
+
     carrera_caracoles(random.choice(caracoles),random.randint(0,3))
+
+def limpiar_pantalla():
+    """Limpia la pantalla de la terminal
+    - Entrada: Ninguna
+    - Salida: Limpia la pantalla
+    - Restricciones: Ninguna
+    """
+
+    match os.name:
+        case "posix":
+            os.system('clear')
+        case "nt":
+            os.system("cls")
+        case _:
+            print('\n' * 100)
 
 def menu_juego():
     """Muestra el menu principal del juego, y da una diferente salida dependiendo de la opcion escogida.
@@ -100,36 +117,41 @@ def menu_juego():
     - Salida: Muestra el menu principal, y luego o muestra las instrucciones, o empieza el juego, o termina el programa.
     - Restricciones: La entrada debe ser 1, 2, o 3.
     """
-        
-    print(""" 
-                                                                                                                                                                                            
- ,-----.                                                   ,--.                                                     ,--.                                                                    
-'  .--./ ,--,--.,--.--.,--.--. ,---. ,--.--. ,--,--.     ,-|  | ,---.      ,---. ,--,--.,--.--. ,--,--. ,---. ,---. |  | ,---.  ,---.                                                       
-|  |    ' ,-.  ||  .--'|  .--'| .-. :|  .--'' ,-.  |    ' .-. || .-. :    | .--'' ,-.  ||  .--'' ,-.  || .--'| .-. ||  || .-. :(  .-'                                                       
-'  '--'\\ '-'  ||  |   |  |   \   --.|  |   \ '-'  |    \ `-' |\   --.    \ `--.\ '-'  ||  |   \ '-'  |\ `--.' '-' '|  |\   --..-'  `)                                                      
- `-----' `--`--'`--'   `--'    `----'`--'    `--`--'     `---'  `----'     `---' `--`--'`--'    `--`--' `---' `---' `--' `----'`----'                                                     
-""")
+
+    limpiar_pantalla()
+    logo_juego = pyfiglet.figlet_format("SnailRace", font="banner")
+    print(logo_juego)
     print("\n")
     
-    respuesta = input("Bienvenido!\nEscriba el numero de la opcion que quiere:\n\n1.Mostrar las intstrucciones\n2.Jugar\n3.Salir del juego\n")
+    respuesta = input("Bienvenido a SnailRace, la carrera de caracoles mas epica del planeta!\nEscriba el numero de la opcion que quiere:\n\n1.Instrucciones\n2.Nueva partida\n3.Que es esta basura?\n4.Salir del juego\n\n")
         
         
     if respuesta == "1":
+        limpiar_pantalla()
         print("""\nEste juego consiste en una carrera de caracoles que compiten para llegar a la meta. 
-Cada correra consta de dos caracoles en carriles paralelos, y cada uno tiene 12 casillas para recorrer. 
-El primer caracol en llegar a la meta gana.""")
+Cada correra consta de dos caracoles en carriles paralelos, y cada uno tiene 32 casillas para recorrer. 
+El primer caracol en llegar a la meta gana.\n""")
+        input("Presione ENTER para volver al menu principal.\n")
         print("\nVolviendo al menu principal...")
         menu_juego()
     
     elif respuesta == "2":
+        print("\n")
         tablero_juego()
     
     elif respuesta == "3":
+        limpiar_pantalla()
+        print("""\nEste juego cuestionable fue el primer proyecto programado que hice en toda mi carrera\ny no me arrepiento (mentira si lo hago) por favor no me juzguen\n""")
+        input("\nPresione ENTER para volver al menu principal.\n")
+        print("\nVolviendo al menu principal...")
+        menu_juego()
+
+    elif respuesta == "4":
         print("\nSaliendo del juego...")
         raise SystemExit
     
-    elif respuesta != "1" or respuesta != "2" or respuesta != "3":
-        print("Error 01: Por favor escoja una de las tres opciones.")
+    elif respuesta != "1" or respuesta != "2" or respuesta != "3" or respuesta != "4":
+        print("Error 01: Por favor escoja una de las opciones.")
         menu_juego()
     
 def iniciar_juego():
